74cc25f08dcbf5e0fd2e8950216adce9c9d16acd
Coleman McFarland <43583445+dontlaugh@users.noreply.github.com>
2020-06-18 15:56:43 -0400

    15 nodes. The bots are attacking. (#19)

--- eks.tf
+++ eks.tf
@@ -14,9 +14,9 @@ module "eks" {
   worker_groups = {
     m5-general = {
       instance_type = "m5.xlarge"
-      desired_size  = 10
-      minimum_size  = 10
-      maximum_size  = 10
+      desired_size  = 15
+      minimum_size  = 15
+      maximum_size  = 15
     }
   }
 }
