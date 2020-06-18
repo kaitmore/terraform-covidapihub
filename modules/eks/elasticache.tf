bffd73084e954a5a8fde5ef865a93d70da5ff230
Kait Moreno <kaitlin.moreno@deciphernow.com>
2020-06-18 10:19:16 -0400

    add subnet (#13)
    * add subnet
    
    * move into module
    
    * add sg

---
+++ modules/eks/elasticache.tf
@@ -0,0 +1,48 @@
+
+resource "aws_elasticache_cluster" "novelcovid_redis" {
+  cluster_id           = "novelcovid-redis"
+  engine               = "redis"
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis3.2"
+  engine_version       = "3.2.10"
+  port                 = 6379
+  subnet_group_name    = aws_subnet.master[0].id
+  security_group_ids   = [aws_security_group.novelcovid_redis_sg.id]
+  depends_on = [
+    aws_subnet.master
+  ]
+}
+
+output "novelcovid_azs" {
+  value = aws_elasticache_cluster.novelcovid_redis.availability_zones
+}
+
+output "novelcovid_cache_nodes" {
+  value = aws_elasticache_cluster.novelcovid_redis.cache_nodes
+}
+
+resource "aws_security_group" "novelcovid_redis_sg" {
+  name        = "novelcovid_redis_sg"
+  description = "Elasticache redis to cluster"
+  vpc_id      = aws_vpc.cluster.id
+
+  ingress {
+    description = "Inbound from VPC"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.cluster.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "novelcovid_redis_sg"
+  }
+}
